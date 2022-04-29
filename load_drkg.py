29463a1233c187847ba01f90e2b00ccb6a337f5c
FMagnani <federico.magnani5@studio.unibo.it>
2022-04-29 16:40:59 +0200

    Added docs

--- load_drkg.py
+++ load_drkg.py
@@ -1,14 +1,14 @@
 from dgl.data.knowledge_graph import KnowledgeGraphDataset
 
-class DRKGDataset(KnowledgeGraphDataset):
-    """
+"""
     Class for using a personal dataset. 
     The dataset should be stored into a folder with name "DRKG", that is in turn into "raw_dir".
     The dataset needs to be composed of the following files:
         - train.txt, test.txt, valid.txt (that in fact are tsv)
         - entities.dict, relations.dict (that also are tsv)
-    """
-    
+"""    
+
+class DRKGDataset(KnowledgeGraphDataset):
     def __init__(self, reverse=True, raw_dir=None, force_reload=False,
                  verbose=True, transform=None):
         name="DRKG"
@@ -22,9 +22,7 @@ class DRKGDataset(KnowledgeGraphDataset):
         return super(DRKGDataset, self).__len__()
 
     def download(self):
-    """
-    We are loading the graph through the code originally used for the built-in datasets, that are donwloaded.
-    Since we already have our dataset in the folder, we disable the download step.
-    """
+#    We are loading the graph through the code originally used for the built-in datasets, that are donwloaded.
+#    Since we already have our dataset in the folder, we disable the download step.
         pass
 
