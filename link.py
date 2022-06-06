2214df163ba2fc39475c06f7e4e61c6e714562ee
Federico Magnani <federico.magnani5@studio.unibo.it>
2022-06-06 13:44:38 +0200

    Simplified code

--- link.py
+++ link.py
@@ -1,16 +1,13 @@
 """
-Differences compared to MichSchli/RelationPrediction
-* Report raw metrics instead of filtered metrics.
-* By default, we use uniform edge sampling instead of neighbor-based edge
-  sampling used in author's code. In practice, we find it achieves similar MRR.
-  
-[Fede]
-Other differences compared to MichSchli/RelationPrediction
-* soft_margin_loss ( = logistic loss) instead of cross entropy. In order to make it comparable to the paper DRKG of Ioannidis.
+Code adapted from https://github.com/dmlc/dgl/blob/master/examples/pytorch/rgcn/link.py
 
-Differences compared to the original example of DGL
-* Pytorch geometric's RGCNConv instead of dgl's implementatino of RGCN
- 
+I tried to:
+- Change the loss function
+- Change the RGCN layer (here imported from 'model'), in particular:
+- - Using only 1 convolution
+- - Using the implementation of PyTorch of the same layer, instead of the implementation of DGL
+- - Transfer learning from the embeddings given by shallow embeddig by DistMult (obtained with DGL-KE)
+But I could not make the model to converge.
 """
 
 import argparse
@@ -18,7 +15,7 @@ import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
 
-from load_drkg import DRKGDataset
+from load_dataset import MyDataset
 from dgl.dataloading import GraphDataLoader
 
 from link_utils import preprocess, SubgraphIterator, calc_mrr, compute_results
@@ -27,7 +24,7 @@ from model import RGCN
 from numpy import isnan, load
 
 class LinkPredict(nn.Module):
-                                  # def: h dim 500, num bases 100          0.2            0.01
+                              # default: h dim 500, num bases 100          0.2            0.01
     def __init__(self, in_dim, num_rels, h_dim=400, num_bases=100, dropout=0.2, reg_param=0.01):
         super(LinkPredict, self).__init__()
         self.rgcn = RGCN(in_dim, h_dim, h_dim, num_rels * 2, regularizer="bdd",
@@ -55,6 +52,7 @@ class LinkPredict(nn.Module):
     def get_loss(self, embed, triplets, labels):
         # each row in the triplets is a 3-tuple of (source, relation, destination)
         score = self.calc_score(embed, triplets)
+        # The loss can be chosen
 #        predict_loss = F.soft_margin_loss(score, labels)
         predict_loss = F.binary_cross_entropy_with_logits(score, labels)
         reg_loss = self.regularization_loss(embed)
@@ -62,11 +60,13 @@ class LinkPredict(nn.Module):
 
 def main(args):
 
-    # Set the absolute path to the dataset folder. The dataset should be formatted into the following files:
+    # Set the absolute path to the dataset folder. 
+    # Inside this folder there must be other folders with the name of the dataset for example "DRKG"
+    # The dataset should be formatted into the following files:
     # train.txt, test.txt, valid.txt, entities.dict, relations.dict (all these are tsv)
     raw_dir = "/mnt/raid1/fede/"
 
-    data = DRKGDataset(reverse=False, raw_dir=raw_dir)
+    data = MyDataset(name="DRKG", reverse=False, raw_dir=raw_dir)
     graph = data[0]
     num_nodes = graph.num_nodes()
     num_rels = data.num_rels
@@ -78,7 +78,7 @@ def main(args):
     valid_nids = th.arange(0, valid_g.num_nodes())
 
     # Set here the batch size (for edge sampling) as sample_size, and the epochs for training
-                                                            # Default: sample_size 30000 num_epochs 6000
+                                                            # Default: sample_size 30000  num_epochs 6000
     subg_iter = SubgraphIterator(train_g, num_rels, args.edge_sampler, sample_size=10000, num_epochs=1000)
     dataloader = GraphDataLoader(subg_iter, batch_size=1, collate_fn=lambda x: x[0])
 
@@ -90,14 +90,6 @@ def main(args):
     # Set LR                              default lr 1e-2
     optimizer = th.optim.Adam(model.parameters(), lr=0.01)
 
-    resumed_epochs = 0
-    model_state_file = 'model_state.pth'
-    resume_training = False
-    if resume_training:
-        th.load(model_state_file)
-        resumed_epochs = 300
-        print("Training will resume from 300 epochs")
-
     if args.gpu >= 0 and th.cuda.is_available():
         device = th.device(args.gpu)
         print("GPU will be employed.")
@@ -106,8 +98,8 @@ def main(args):
         print("CPU will be employed.")
     model = model.to(device)
 
-    hits = 0
-    
+    best_mrr = 0
+    model_state_file = 'model_state.pth'    
     for epoch, batch_data in enumerate(dataloader):
         model.train()
 
@@ -117,20 +109,8 @@ def main(args):
         edges = edges.to(device)
         labels = labels.to(device)
 
-        if epoch==1:
-            embed = th.from_numpy(load("Test/DistMult_entity.npy"))
-            embed = embed.to(device)
-            state_dict = model.state_dict()
-            state_dict['w_relation'] = th.from_numpy(load("Test/DistMult_relation.npy"))
-            model.load_state_dict(state_dict)
-        else:
-            embed = model(g, train_nids)
-        loss = model.get_loss(embed, edges, labels)
-        
-        if isnan(loss.item()):
-            print("Loss is nan!")
-            break
-                
+        embed = model(g, train_nids)
+        loss = model.get_loss(embed, edges, labels)                
         optimizer.zero_grad()
         loss.backward()
         nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0) # clip gradients
@@ -140,55 +120,37 @@ def main(args):
 
         if (epoch+1)%100==0:
             
-            # Compute hits - on cpu
+            # perform validation on CPU because full graph is too large
             model = model.cpu()
             model.eval()
     
-            # Evaluation on the validation
-            embed = model(valid_g, valid_nids)
-    
-            sources, targets = valid_g.edges()
-            rels = valid_g.edata["_TYPE"]
-            triplets = th.stack([sources,rels,targets], dim=1)
-    
-            with th.no_grad():
-                scores = model.calc_score(embed, triplets)
-            scores = scores.numpy()
+            print("start eval")
+            embed = model(test_g, test_nids)
+            mrr = calc_mrr(embed, model.w_relation, test_mask, triplets,
+                           batch_size=500, eval_p=args.eval_protocol)
+            # save best model
+            if best_mrr < mrr:
+                best_mrr = mrr
+                th.save({'state_dict': model.state_dict(), 'epoch': epoch}, model_state_file)
 
-            results = compute_results(raw_dir, scores)
-
-            # save model if it works
-            if not results.empty:
-                print(results)
-                th.save({'state_dict': model.state_dict(), 'epoch': epoch}, 'model_state_'+str(epoch)+'.pth')
-    
-                name = "Hits100_"+str(epoch)+".csv"
-                results[["CompoundName", "Rank", "score", "Target_Disease", "Rel"]].to_csv(name)
-    
             model = model.to(device)
-            
-                
-    # Compute hits - on cpu
-    print("Final evaluation")
-    model = model.cpu()
-    model.eval()
-    embed = model(valid_g, valid_nids)
-    
-    sources, targets = valid_g.edges()
-    rels = valid_g.edata["_TYPE"]
-    triplets = th.stack([sources,rels,targets], dim=1)
-    
-    with th.no_grad():
-        scores = model.calc_score(embed, triplets)
-    scores = scores.numpy()
 
-    results = compute_results(raw_dir, scores)
+            
+    print("Start testing:")
+    # use best model checkpoint
+    checkpoint = th.load(model_state_file)
+    model = model.cpu() # test on CPU
+    model.eval()
+    model.load_state_dict(checkpoint['state_dict'])
+    print("Using best epoch: {}".format(checkpoint['epoch']))
+    embed = model(test_g, test_nids)
+    calc_mrr(embed, model.w_relation, test_mask, triplets,
+             batch_size=500, eval_p=args.eval_protocol)
 
     # save model if it works
     if not results.empty:
         print(results)    
         th.save({'state_dict': model.state_dict(), 'epoch': epoch}, 'model_state_'+str(epoch)+'.pth')
-
 
 
 if __name__ == '__main__':
